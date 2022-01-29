7a00fd66afb935afa9f9734ad93eeb9f4d58266e
Exxcqz <electro.style72@gmail.com>
2022-01-29 15:29:58 +0600

    add models

---
+++ ImageSearch/Models/SearchResult.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResult.swift
+//  ImageSearch
+//
+//  Created by Nikita Gavrikov on 29.01.2022.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    let total: Int
+    let totalPages: Int
+    let results: [PhotoInfo]
+
+    enum CodingKeys: String, CodingKey {
+        case total
+        case totalPages = "total_pages"
+        case results
+    }
+}
